98db8cbcc1562d529c754b71ba0e41c2add76919
Cameron Reikes <cameronreikes@gmail.com>
2021-11-20 17:46:27 -0800

    Changes from Elegantbeef

--- bf.nim
+++ bf.nim
@@ -4,20 +4,14 @@ import std/tables
 let params = os.commandLineParams()
 if params.len() < 1 or params.len() > 2:
     echo "Usage: bf.nim [brainfuck program file] [optional input to program file]"
-    quit(system.QuitFailure)
+    quit(QuitFailure)
 
 proc getText(fileName: string): string =
-    var f: File
-    if open(f, fileName):
-        defer: close(f)
-        try:
-            result = f.readAll()
-        except:
-            echo "Could not read file: ", getCurrentExceptionMsg()
-            quit(system.QuitFailure)
-    else:
-        echo "Could not open file '", fileName, "'"
-        quit(system.QuitFailure)
+  try:
+    readFile(fileName)
+  except Exception as e:
+   echo "Could not read file: ", e.msg
+   quit(QuitFailure)
         
 
 let text = getText(params[0])
@@ -31,11 +25,11 @@ var instructionIndex = 0
 var inputIndex = 0
 var memoryIndex = 0
 
-proc curMem(): ptr uint8 =
+proc curMem(): var uint8 =
     if memoryIndex >= 0:
-        result = addr(rightMemory[memoryIndex])
+        result = rightMemory[memoryIndex]
     else:
-        result = addr(leftMemory[-1 * memoryIndex - 1])
+        result = leftMemory[-1 * memoryIndex - 1]
 
 proc fixMemory(): void =
     while memoryIndex >= 0 and memoryIndex >= rightMemory.len():
@@ -61,32 +55,35 @@ if openBraceStack.len() > 0:
 
 var steps = 0
 while instructionIndex < text.len():
-    let cur = text[instructionIndex]
-    if cur == '+':
-        curMem()[] += 1
-    elif cur == '-':
-        curMem()[] -= 1
-    elif cur == '.':
-        stdout.write char(curMem()[])
-    elif cur == ',':
+    let cur = text[instructionIndex] 
+    case cur
+    of '+':
+        curMem() += 1
+    of '-':
+        curMem() -= 1
+    of '.':
+        stdout.write char(curMem())
+    of ',':
         if inputIndex < inputText.len():
-            curMem()[] = uint8(inputText[inputIndex])
+            curMem() = uint8(inputText[inputIndex])
             inputIndex += 1
         else:
             echo "\nRan out of input to give the brainfuck program"
-            quit(system.QuitSuccess)
-    elif cur == '>':
+            quit(QuitSuccess)
+    of '>':
         memoryIndex += 1
         fixMemory()
-    elif cur == '<':
+    of '<':
         memoryIndex -= 1
         fixMemory()
-    elif cur == '[':
-        if curMem()[] == 0:
+    of '[':
+        if curMem() == 0:
             instructionIndex = openBraceMap[instructionIndex]
-    elif cur == ']':
-        if curMem()[] != 0:
+    of ']':
+        if curMem() != 0:
             instructionIndex = closeBraceMap[instructionIndex]
+    else:
+        discard
     
     steps += 1
     instructionIndex += 1
